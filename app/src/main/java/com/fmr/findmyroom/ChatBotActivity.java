a2c3930bedae50bc43e98619b3e50a1f9c57ce70
pvbgayashan <pvbgayashan@gmail.com>
2018-03-28 15:41:02 +0530

    UPDATE: chat view with text input area and change the font size

--- app/src/main/java/com/fmr/findmyroom/ChatBotActivity.java
+++ app/src/main/java/com/fmr/findmyroom/ChatBotActivity.java
@@ -1,26 +1,53 @@
 package com.fmr.findmyroom;
 
+import android.content.Context;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ChatBotActivity extends AppCompatActivity {
 
-    List<ChatModel> firstChat = new ArrayList<>();
+    private List<ChatModel> firstChat = new ArrayList<>();
+    private Context thisContext = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_bot);
 
+        // set up initial message list
         setUpMessage();
 
+        // setup list view
         ListView chatListView = findViewById(R.id.chatListView);
-        ChatMessageAdapter chatMessageAdapter = new ChatMessageAdapter(firstChat, this);
+        ChatMessageAdapter chatMessageAdapter = new ChatMessageAdapter(firstChat, thisContext);
         chatListView.setAdapter(chatMessageAdapter);
+
+        // handle message sending button event
+        FloatingActionButton msgSendBtn = findViewById(R.id.msgSendBtn);
+        msgSendBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                // get the input message
+                TextView inputMsgTxtView = findViewById(R.id.inputMessage);
+                String inputMessage = inputMsgTxtView.getText().toString();
+
+                // set the message to model
+                firstChat.add(new ChatModel(inputMessage, false));
+
+                // setup list view
+                ListView chatListView = findViewById(R.id.chatListView);
+                ChatMessageAdapter chatMessageAdapter = new ChatMessageAdapter(firstChat, thisContext);
+                chatListView.setAdapter(chatMessageAdapter);
+            }
+        });
     }
 
     private void setUpMessage() {
