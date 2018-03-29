1851982af0e15805212de4ada27c539e5a382d08
pvbgayashan <pvbgayashan@gmail.com>
2018-03-29 06:19:39 +0530

    ADD: tab view to user connection

--- app/src/main/java/com/fmr/findmyroom/UserConnectionActivity.java
+++ app/src/main/java/com/fmr/findmyroom/UserConnectionActivity.java
@@ -1,13 +1,50 @@
 package com.fmr.findmyroom;
 
+import android.support.design.widget.TabLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
 
 public class UserConnectionActivity extends AppCompatActivity {
+
+    private static final String TAG = "UserConnectionActivity";
+    private SectionsPageAdapter mSectionsPageAdapter;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_connection);
+
+        mSectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
+
+        mViewPager = findViewById(R.id.container);
+        setupViewPager(mViewPager);
+
+        TabLayout tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+    }
+
+    public void setupViewPager(ViewPager viewPager) {
+        SectionsPageAdapter sectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
+
+        sectionsPageAdapter.addFragment(new LoginFragment(), "LOGIN");
+        sectionsPageAdapter.addFragment(new SignupFragment(), "SIGN UP");
+
+        viewPager.setAdapter(sectionsPageAdapter);
     }
 }
