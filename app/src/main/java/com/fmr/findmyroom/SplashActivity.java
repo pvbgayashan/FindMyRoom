03be083da472eac6e139211da5442708bbc78611
pvbgayashan <pvbgayashan@gmail.com>
2018-05-18 01:30:48 +0530

    UPDATE: splash screen and launcher icon

--- app/src/main/java/com/fmr/findmyroom/SplashActivity.java
+++ app/src/main/java/com/fmr/findmyroom/SplashActivity.java
@@ -6,7 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -16,15 +15,12 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
         ImageView splashImageView;
-        TextView splashTextView;
         Animation splashAnimation;
 
         splashImageView = findViewById(R.id.splashImage);
-        splashTextView = findViewById(R.id.splashTxt);
         splashAnimation = AnimationUtils.loadAnimation(this, R.anim.splash_transition);
 
         // animate splash screen
-        splashTextView.startAnimation(splashAnimation);
         splashImageView.startAnimation(splashAnimation);
 
         // navigate to home screen
